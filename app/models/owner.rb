43ba5e5b3b76c93e810acf289edd0c6fcc8cb8e0
Andres Garcia <drawingonideas@gmail.com>
2014-06-18 23:54:48 +0000

    fix gemfile

---
+++ app/models/owner.rb
@@ -0,0 +1,20 @@
+class Owner
+  def name
+    name = 'Andy'
+  end
+  
+  def birthdate
+    birthdate = Date.new(2000, 01, 12)
+  end
+  
+  def countdown
+    today = Date.today
+    birthday = Date.ndw)today.year, birthdate.month, birthdate.day)
+
+if birthday > today
+  countdown = (birthday - today).to_i
+else
+  countdown = (borthday - today).to_i
+end
+end   
+end
