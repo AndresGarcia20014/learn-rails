efae759cd1baea3d2800b28edbb05476173c2710
AndresGarcia20014 <drawingonideas@gmail.com>
2014-06-28 22:32:58 +0000

    contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,21 @@
 class ContactsController < ApplicationController
   
-  def process form
+  def new
+    @contact = Contact.new 
   end
+  def create 
+    @contact = Contact.new(secure_params)
+    if @contact.valid?
+      # save the data
+      # send message 
+      flash[:notice] = "message sent from #{@contact.name}"
+      redirect_to root_path
+    end
   
+  end 
+    private
+    
+  def secure_params
+      params.require(:contact).permit(:name, :email, :content)
+  end
 end
