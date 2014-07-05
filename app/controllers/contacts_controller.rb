39420979cf41f65b1fd4a9c09a2e24c0fd35698a
AndresGarcia20014 <drawingonideas@gmail.com>
2014-07-05 22:38:20 +0000

    contact form functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,23 @@
 class ContactsController < ApplicationController
-  
   def new
-    @contact = Contact.new 
+    @contact = Contact.new
   end
-  def create 
-    @contact = Contact.new(secure_params)
-    if @contact.valid?
-      # save the data
-      # send message 
-      flash[:notice] = "message sent from #{@contact.name}"
-      redirect_to root_path
-    end
   
-  end 
-    private
-    
+  def create
+    @contact = Contact.new(secure_params) 
+    if @contact.valid?
+      @contact.update_spreadsheet
+      UserMailer.contact_email(@contact).deliver 
+      flash[:notice] = "Message sent from #{@contact.name}."
+      redirect_to root_path
+    else
+      render :new
+    end 
+  end
+  
+  private
+  
   def secure_params
-      params.require(:contact).permit(:name, :email, :content)
+    params.require(:contact).permit(:name, :email, :content)
   end
 end
